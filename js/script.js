e0019f585c232830ac4f2e11ba08fa41ef30f599
shilpi <shilpikharya@gmail.com>
2021-02-11 11:34:37 -0800

    some steps added in js and link to js in index file added

---
+++ js/script.js
@@ -0,0 +1,51 @@
+/*1. Convert the following highlighted identifiers to Camel Case notation */
+
+/* Declaring in Camel Case Notation */
+// let someMonth;
+// function theMonth();
+// let currentMonth;
+// let summerMonth;
+// let myLibraryFunction;
+
+/**************************************************************************** */
+/* 2.    Give me an example of a numeric literal expression, a string literal expression, a Boolean literal expression, and a null literal expression. */
+
+// let a = 5.4; //numeric literal
+// let b = "string "; // string literal
+// let c = false; // boolean literal
+// let d = null; // literal null value
+
+/************************************************************************************* */
+/* 3.    Give me two examples of complex / variable expressions. */
+
+// let complex1 = 3 * (4 / 5) + 6; //
+// let complex2 = complex1 + (8 * 5 );
+
+/************************************************************* */
+/* 4.    Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers. */
+
+// let strFirstName;
+// let strLastName;
+// let strAddress;
+// let strCity;
+// let strState;
+// let strZipCode;
+// let numYourAge;
+// let strReferralSource;
+// let strMayWeContactYou;
+
+/***************************************************************** */
+/* 5.    Take the 3 of the 9 variables that you created above and demonstrate 3 ways for declaring and assigning values to those variables. */
+
+// let strFirstName ="Stanley";
+// let strCity = "San Diego";
+// let strState = "CA";
+
+/***************************************************************** */
+/* 6.    Create a variable. Add a number and a string and display the coerced result in the browser’s console window.  */ 
+
+// let var1 ;
+// var1 = 1 + " is number of records";
+// console.log(var1);
+
+
