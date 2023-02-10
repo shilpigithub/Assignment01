59a03dd37bf4a0ecfd46cccc79c74081931bb09d
shilpigithub <shilpis@marsreturnship.com>
2023-02-09 22:01:58 -0800

    first commit

---
+++ js/script.js
@@ -0,0 +1,133 @@
+// 1.)
+// let someMonth
+// function theMonth()
+// let currentMonth
+// let summerMonth
+// let myLibraryFunction
+
+// 2.)
+// let n = 4.5         //numeric literal
+// let aStr = 'SDCCD'     //string literal
+// let abool = false       //boolean literal
+// let t = null        //null literal
+
+// 3.)
+// let a = 4 * 5 * (3 + 4) 
+
+// 4.)
+// Declare (but do not assign) 9 variables for the following identifiers: First Name, Last Name, Address, City, State, Zip Code, Your Age, Referral Source, May We Contact You. Use Camel Casing and Hungarian Notation when naming your identifiers.
+
+// let firstName
+// let lastName
+// let address
+// let city
+// let state
+// let intZipCode
+// let intAge
+// let strReferralSource
+// let strMayWeContactYou
+
+// 5.)
+
+// let intZipCode = 92127
+// let intAge = 23
+// let strReferralSource = 'LinkedIn'
+
+// 6.)
+
+// let x = 6
+// console.log('I have ' + x + ' years of experience as developer')
+
+// 7.)
+// let a = true + ' hello'
+// document.write(`<h1> ${a}</h1>`)
+
+// let b = 10 + false 
+// document.write(`<h1>${b} </h1>`)
+
+// 8.)
+
+// let someString = 'Who once said, "Only two things are infinite, the universe and human stupidity, and I\'m not sure about the former."'
+// console.log(someString)
+
+// 9.)
+// let b = null
+// console.log ("b: " + b)
+
+// let a
+// console.log ("a: " + a)
+
+// 10.)
+//Use the unary typeof operator on various literals to return the following types within the console window: string, number, Boolean, object, and undefined.
+
+// console.log(typeof 'Hi')
+// console.log(typeof 30)
+// console.log(typeof true)
+// console.log(typeof Math)
+// console.log(typeof a)
+
+// 11.)
+
+//  alert('Hello ' + 'Shilpi' + ', welcome to the JavaScript class!')
+
+// 12.)
+
+// let name = 'Shilpi'
+// alert('Hello ' + name + ', welcome to the JavaScript class!')
+
+// 13.)
+// let name = 'Shilpi'
+// let course = "JavaScript"
+// alert('Hello ' + name + ', welcome to the '+ course + ' class!')
+
+// 14.)
+// let name = 'Shilpi'
+// let course = "JavaScript"
+// alert('Hello ' + name + '.\nWelcome to the '+ course + ' class!')
+
+// 15.)
+// let name = prompt('Enter your name')
+// let course = "JavaScript"
+// alert('Hello ' + name + '.\nWelcome to the '+ course + ' class!')
+
+// 16.)
+
+// let name = prompt('Enter your name')
+// let course = prompt("Enter the course you are taking ")
+// alert('Hello ' + name + '.\nWelcome to the '+ course + ' class!')
+
+// 17.)
+
+// let x = 10
+// let y = 20
+// console.log("sum: " + (x+y))
+
+// 18.)
+
+// let x = 20
+// x += 20
+// console.log(x)
+
+// 19.)
+
+// let x = 20
+// x *= 5
+// console.log(x)
+
+// 20.)
+
+// let x = 20%3
+// x /= 1
+// console.log(x)
+
+// 21.)
+
+// let x = 10
+// let y = 20
+// console.log(x==10 && y == 20)
+
+
+// 22.)
+// let x = 5
+// let y = 7
+// console.log(x<10 && y > 20)
